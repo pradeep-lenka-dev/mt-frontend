58d8d8433c06a28bb1bb3bcb51fc937f3b055e1b
pradeep-lenka-dev <pradeeplenka6@gmail.com>
2024-02-13 00:17:21 +0530

    dashbord setup

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewsRoutingModule } from './views-routing.module';
 import { DashobrdComponent } from './dashobrd/dashobrd.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
+import { RouterModule, Routes } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    DashobrdComponent
+    DashobrdComponent,
+    HeaderComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
+    TooltipModule,
+    RouterModule,
+     //Routes,
     ViewsRoutingModule
   ]
 })
